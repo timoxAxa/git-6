84fd7ff9baab83f6ea941d6e1fe34d44dcd0b9f5
timoxAxa <kushmamatovtimur@gmail.com>
2023-01-05 10:29:54 +0500

    first commit

---
+++ scriptt.js
@@ -0,0 +1,176 @@
+// Отфильтровать задачи в переменные а и b
+    // Сохранить количество в ключе count
+    // Сохранить сами задачи в массиве-ключе arr
+    let arr = [
+        {
+            "userId": 1,
+            "id": 1,
+            "title": "delectus aut au`tem",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 2,
+            "title": "quis ut nam facilis et officia qui",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 3,
+            "title": "fugiat veniam minus",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 4,
+            "title": "et porro tempora",
+  
+            "completed": 
+        },
+        {
+            "userId": 1,
+            "id": 5,
+            "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 6,
+            "title": "qui ullam ratione quibusdam voluptatem quia omnis",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 7,
+            "title": "illo expedita consequatur quia in",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 8,
+            "title": "quo adipisci enim quam ut ab",
+            "completed": true
+        },
+        {
+            "userId": 1,
+   
+            "id": 9,
+            "title": "molestiae perspiciatis ipsa",
+            "completed": false
+        },
+        {
+            "userId": 1,
+            "id": 10,
+            "title": "illo est ratione doloremque quia maiores aut",
+            "completed": true
+        },
+    ]
+    let a = {
+        count: 0,
+        arr: []
+    }
+   
+    let b = {
+        count: 0,
+        arr: []
+    }
+
+    for(const item of arr)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arrr = new Array();
+// let arr = []
+
+// console.log(arr, arrr);
+
+// let arr = [
+//     'ваня',
+//     'сеня',
+//     'тима'
+// ]
+
+// console.log(arr);
+
+
+// // Различные типы значений
+// let arrTwo = [
+//     "Коля",
+//     {
+//         type: "JS",
+//         age: 21
+//     },
+//     true,
+//     function (){
+//         console.log('Hello i am Kolya');
+//     }
+// ];
+// console.log(arrTwo[1].type + ' ' + arrTwo[1].age + ' ' + arrTwo[3]);
+
+// // Многомерные массивы
+// let matrix = [
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9]
+// ];
+// console.log(matrix[0]+',' + matrix[1] +','+ matrix[2]);
+
+// let arr = ['alex','lexa','tima',];
+// console.log(arr);
+
+// let arrNew = arr;
+
+// arrNew.length = 2;
+// console.log(arr);
+
+// let arr = ['alex','lexa','tima',];
+
+// arr[0] = 'toma';
+// console.log(arr);
+
+// arr[3] = 'alex';
+// console.log(arr);
+
+// let arr = ['alex','lexa','tima',];
+// arr.push('doshik','rolton',)
+// console.log(arr);
+
+
+// let arr = ['alex','lexa','tima',];
+// arr.shift();
+// console.log(arr);
+
+
+
+// let arr = ['alex','lexa','tima',];
+// arr.pop();
+// console.log(arr);
+
+
+
+// let arr = ['alex','lexa','tima',];
+// arr.unshift('mexa');
+// console.log(arr);
+
+// let arr = ['alex','lexa','tima',];
+// delete arr[1];
+// console.log(arr);
+// console.log(arr[1]);
+// console.log(arr.length);
+
+
